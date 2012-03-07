f09b06d5dfdf37bc8fbfef94eb2f6d8f6b89f5e7
Richard Chen <richardchen331@gmail.com>
2012-03-06 20:42:07 -0800

    starter code

---
+++ Main.cpp
@@ -0,0 +1,46 @@
+#include "Framework.h"
+#include "Shader.h"
+
+// Note: See the SMFL documentation for info on setting up fullscreen mode
+// and using rendering settings
+// http://www.sfml-dev.org/tutorials/1.6/window-window.php
+sf::WindowSettings settings(24, 8, 2);
+sf::Window window(sf::VideoMode(800, 600), "CS248 Rules!", sf::Style::Close, settings);
+ 
+// This is a clock you can use to control animation.  For more info, see:
+// http://www.sfml-dev.org/tutorials/1.6/window-time.php
+sf::Clock clck;
+
+
+int main(int argc, char** argv) {
+    // Put your game loop here (i.e., render with OpenGL, update animation)
+    while (window.IsOpened()) {
+        window.Display();
+    }
+
+    return 0;
+}
+
+
+
+void initOpenGL() {
+    // Initialize GLEW on Windows, to make sure that OpenGL 2.0 is loaded
+#ifdef FRAMEWORK_USE_GLEW
+    GLint error = glewInit();
+    if (GLEW_OK != error) {
+        std::cerr << glewGetErrorString(error) << std::endl;
+        exit(-1);
+    }
+    if (!GLEW_VERSION_2_0 || !GL_EXT_framebuffer_object) {
+        std::cerr << "This program requires OpenGL 2.0 and FBOs" << std::endl;
+        exit(-1);
+    }
+#endif
+
+    // This initializes OpenGL with some common defaults.  More info here:
+    // http://www.sfml-dev.org/tutorials/1.6/window-opengl.php
+    glClearDepth(1.0f);
+    glClearColor(0.15f, 0.15f, 0.15f, 1.0f);
+    glEnable(GL_DEPTH_TEST);
+    glViewport(0, 0, window.GetWidth(), window.GetHeight());
+}
